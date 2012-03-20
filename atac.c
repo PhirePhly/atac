a703b3d122a85e207bc3dafc72de914604128da3
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2012-03-20 15:57:04 -0700

    Fixed copyright information

--- atac.c
+++ atac.c
@@ -1,10 +1,9 @@
 // atac.c -- ASCII to ASCII Converter
-// Copyright 2007, 2008 - Kenneth Finnegan.
+// Copyright 2007,08,12 - Kenneth Finnegan.
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// it under the terms of the GNU General Public License version 3 as 
+// published by the Free Software Foundation.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
